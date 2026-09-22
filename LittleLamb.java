4c25cef835fdedf1dc822a406e356e454da91d6e
Jayden Chang <45678161+Turphin@users.noreply.github.com>
2026-09-22 12:18:49 -0700

    Create LittleLamb.java

---
+++ LittleLamb.java
@@ -0,0 +1,11 @@
+public class LittleLamb {
+   public static void main(String[] args) {
+      System.out.print("Mary had a ");
+      maryPet();
+      maryPet();
+      maryPet();
+   }
+   public static void maryPet() {
+      System.out.println("Honey-baked ham");
+   }
+}
